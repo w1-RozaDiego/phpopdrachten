f8fdce74971ca918a8aa61620e82abdd62d04079
Diego Roza <diego.roza@edu-kw1c.nl>
2020-02-12 11:44:49 +0100

    Index na afmaken en linken van opdracht21.php

---
+++ hoofdstuk2/index.php
@@ -0,0 +1,77 @@
+<?php/*
+ * User: Diego Roza
+ * Date: 10/02/2020
+ * Time: 3:20 PM
+ * File: index.php
+ */ ?>
+
+<link href="stylesheet.css" type="text/css" rel="stylesheet">
+
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2 id="menu">Menu</h2>
+    <ul>
+        <li id="hfd">Hoofdstuk 2</li>
+        <ul>
+            <li>
+                <a href="opdracht21.php">Opdracht 2.1</a>
+            </li>
+            <li>
+                <a href="">Opdracht 2.2</a>
+            </li>
+        </ul>
+    </ul>
+    <ul>
+        <li id="hfd">Hoofdstuk 3</li>
+        <ul>
+            <li>
+                <a href="">Opdracht 3.1</a>
+            </li>
+            <li>
+                <a href="">Opdracht 3.2</a>
+            </li>
+            <li>
+                <a href="">Opdracht 3.3</a>
+            </li>
+        </ul>
+    </ul>
+    <ul>
+        <li id="hfd">Hoofdstuk 4</li>
+        <ul>
+            <li>
+                <a href="">Opdracht 4.1</a>
+            </li>
+            <li>
+                <a href="">Opdracht 4.2</a>
+            </li>
+            <li>
+                <a href="">Opdracht 4.3</a>
+            </li>
+            <li>
+                <a href="">Opdracht 4.4</a>
+            </li>
+        </ul>
+    </ul>
+    <ul>
+        <li id="hfd">Hoofdstuk 5</li>
+        <ul>
+            <li>
+                <a href="">Opdracht 5.1</a>
+            </li>
+            <li>
+                <a href="">Opdracht 5.2</a>
+            </li>
+            <li>
+                <a href="">Opdracht 5.3</a>
+            </li><li>
+                <a href="">Opdracht 5.4</a>
+            </li>
+
+        </ul>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
--- hoofdstuk2/opdracht21.php
+++ hoofdstuk2/opdracht21.php
@@ -1,1 +1,6 @@
 <?php
+
+
+echo "<p>Hello World!</p>"
+?>
+<a href="index.php">Terug</a>
