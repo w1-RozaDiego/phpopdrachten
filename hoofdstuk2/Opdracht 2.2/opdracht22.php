3bc5e943b3d387b88aece9a01f8176ef2428360a
Diego Roza <diego.roza@edu-kw1c.nl>
2020-02-12 12:28:08 +0100

    HTML inhoud toegevoegd

--- hoofdstuk2/Opdracht 2.2/opdracht22.php
+++ hoofdstuk2/Opdracht 2.2/opdracht22.php
@@ -4,7 +4,7 @@
     <head>
         <meta name="description" content="">
         <meta name="keywords" content="">
-        <meta charset="utf-8"?
+        <meta charset="utf-8">
     </head>
 </head>
 <body>
