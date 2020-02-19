2707cf1b598bd10fd198b46ad5c995975069c8a8
Diego Roza <diego.roza@edu-kw1c.nl>
2020-02-19 12:03:56 +0100

    Opdracht helemaal afgerond

--- Hoofdstuk 3/Opdracht32.php
+++ Hoofdstuk 3/Opdracht32.php
@@ -10,6 +10,7 @@
 </head>
 <body>
 <?php
+
 $trafficLightColor = ("green");
 $ambulanceComing = false;
 $driveOn = true;
@@ -25,6 +26,31 @@ else
     echo "<h2 id='redorange'>U moet stoppen.</h2>";
 }
 
+
+
+
+
+$countryName = "";
+$currentAge = 19;
+
+if ($countryName == "Zweden" && $currentAge >= 20 ||
+    $countryName == "Nederland" && $currentAge >= 18 ||
+    $countryName == "Cyprus" && $currentAge >= 17 ||
+    $countryName == "Bulgarije" && $currentAge >= 18 ||
+    $countryName == "België" && $currentAge >= 18)
+{
+    echo "Je woont in ".$countryName." en bent ".$currentAge." jaar oud <br> <br>";
+    echo "Je mag hier sterke alcohol drinken";
+}
+else if ($countryName == "Zweden" && $currentAge >= 18 ||
+    $countryName == "Nederland" && $currentAge >= 18 ||
+    $countryName == "Cyprus" && $currentAge >= 17 ||
+    $countryName == "Bulgarije" && $currentAge >= 18 ||
+    $countryName == "België" && $currentAge >= 16)
+{
+    echo "Je woont in ".$countryName." en bent ".$currentAge." jaar oud <br> <br>";
+    echo "Je mag hier zwakke alcohol drinken";
+}
 ?>
 </body>
 </html>
