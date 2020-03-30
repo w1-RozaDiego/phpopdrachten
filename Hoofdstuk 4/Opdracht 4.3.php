0fdf66a372d3c302dd80de2ba180f7f872da60e3
Diego Roza <diego.roza@edu-kw1c.nl>
2020-03-30 16:20:59 +0200

    Opdracht 4.3 volledig afgerond

---
+++ Hoofdstuk 4/Opdracht 4.3.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: Diego Roza
+ * Date: 30-3-2020
+ * Time: 16:13
+ * File: Opdracht 4.3.php
+ */
+
+$opdracht = "Opdracht 4.3";
+include '../includes/header.php'; ?>
+<?php include 'script43.php'; ?>
+<body>
+<h1>
+    <?php echo $task2 ?>
+</h1>
+<h1>
+    <?php echo $task3 ?>
+</h1>
+<?php echo $task4; echo $task5; echo $task6; echo $task7; ?>
+<br>
+<a href="../index.php">Terug</a>
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
+
---
+++ Hoofdstuk 4/script43.php
@@ -0,0 +1,121 @@
+<?php
+$task2 = "Lancering in: ";
+$counter = 19;
+
+while   ($counter > -1)
+{
+    $task2 = $task2.$counter;
+    if ($counter > 0)
+    {
+        $task2 .= ",";
+    }
+    $counter--;
+}
+
+$task3 = "Lancering in: ";
+
+for ($counter = 19; $counter > -1;)
+{
+    $task3 = $task3 . $counter;
+    if ($counter > 0)
+    {
+        $task3 .= ",";
+    }
+    $counter--;
+}
+
+$task4 = "";
+$counter2 = 1;
+
+while ($counter2 < 7)
+{
+    $task4 .= "<h1>dit is de ".$counter2."e iteratie</h1>";
+    $counter2++;
+}
+
+$task5 = "";
+
+for ($counter2 = 1; $counter2 < 11;)
+{
+    if ($counter2 == 1)
+    {
+        $task5 .= "<table><tr>";
+    }
+    $task5 .= "<td>dit is de ".$counter2."e iteratie</td>";
+    if ($counter2 == 10)
+    {
+        $task5 .= "</tr></table>";
+    }
+    $counter2++;
+}
+$task6 = "";
+$YearDate = 2020;
+
+while ($YearDate > 1999)
+{
+    if ($YearDate == 2020)
+    {
+        $task6 = "<p> in ".$YearDate." ben/word ik 20 jaar oud</p>";
+    }
+    else if ($YearDate > 2000)
+    {
+        $Age = $YearDate - 2000;
+        $task6 .= "<p> in ".$YearDate." was ik ".$Age." jaar oud</p>";
+    }
+    else
+    {
+        $task6 .= "<p> in ".$YearDate." ben ik geboren</p>";
+    }
+    $YearDate--;
+}
+
+$task7 = "";
+$YearDate = 2020;
+
+while ($YearDate > 1999)
+{
+    if ($YearDate == 2020)
+    {
+        $task7 .= "<p> in ".$YearDate." ben ik 20 en word ik volwassenen</p>";
+    }
+    else if ($YearDate == 2019)
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p> in ".$YearDate." word ik $Age </p>";
+    }
+    else if ($YearDate == 2018)
+    {
+        $task7 .= "<p> in ".$YearDate." ben ik 18 en word ik een adolescent</p>";
+    }
+    else if ($YearDate == 2012)
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p>In $YearDate was ik $Age jaar oud en werd ik een puber </p>";
+    }
+    else if ($YearDate == 2008)
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p>In $YearDate was ik $Age jaar oud en werd ik een tiener </p>";
+    }
+    else if ($YearDate == 2004)
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p>In $YearDate was ik $Age jaar oud en werd ik een kleuter </p>";
+    }
+    else if ($YearDate == 2002)
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p>In $YearDate was ik $Age jaar oud en werd ik een peuter </p>";
+    }
+    else if ($YearDate == 2000)
+    {
+        $task7 .= "<p> in ".$YearDate." ben ik geboren en was ik een baby</p>";
+    }
+    else
+    {
+        $Age = $YearDate - 2000;
+        $task7 .= "<p> in ".$YearDate." was ik ".$Age." jaar oud</p>";
+    }
+    $YearDate--;
+}
+?>
